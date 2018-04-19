dd7ac45f6f7e4190113944a0743465dc441b04b0
Chris Cerk <chrcerk@umich.edu>
2018-04-19 06:27:33 -0400

    unsuccessful bug fix

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const angularUniversal = require("angular-universal-express-firebase");
-global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-exports.ssr = angularUniversal.trigger({
-    index: __dirname + '/dist/index.html',
-    main: __dirname + '/dist-server/main.bundle',
-    enableProdMode: true,
-    cdnCacheExpiry: 86400,
-    browserCacheExpiry: 86400 });
+require("zone.js/dist/zone-node");
+require("reflect-metadata");
+const functions = require("firebase-functions");
+const express = require("express");
+const platform_server_1 = require("@angular/platform-server");
+const core_1 = require("@angular/core");
+const fs = require("fs");
+core_1.enableProdMode();
+const document = fs.readFileSync(__dirname + '/dist/index.html', 'utf8');
+const AppServerModuleNgFactory = require(__dirname + '/dist-server/main.bundle');
+const app = express();
+app.get('**', (req, res) => {
+    const url = req.path;
+    platform_server_1.renderModuleFactory(AppServerModuleNgFactory, { document, url })
+        .then(html => {
+        res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+        res.send(html);
+    })
+        .catch(e => {
+        console.log(e);
+    });
+});
+exports.ssr = functions.https.onRequest(app);
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,42 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as path from 'path';
+import { renderModuleFactory } from '@angular/platform-server';
 import { enableProdMode } from '@angular/core';
-import * as angularUniversal from 'angular-universal-express-firebase';
-export let ssr = angularUniversal.trigger({
-    index: __dirname + '/dist-server/index.html',
-    main: __dirname + '/dist-server/main.bundle',
-    enableProdMode: true,
-    cdnCacheExpiry: 86400,
-    browserCacheExpiry: 86400
+import * as fs from 'fs';
+
+enableProdMode();
+
+const document = fs.readFileSync(
+        __dirname + '/dist/index.html',
+        'utf8'
+);
+
+const AppServerModuleNgFactory = require(
+    __dirname + '/dist-server/main.bundle'
+);
+
+const app = express();
+
+app.get('**', (req, res) => {
+    const url = req.path;
+
+    renderModuleFactory(
+        AppServerModuleNgFactory,
+        {document, url}
+    )
+    .then( html => {
+        res.set(
+            'Cache-Control',
+            'public, max-age=600, s-maxage=1200'
+        );
+        res.send(html);
+    })
+    .catch(e => {
+        console.log(e);
+    });
 });
+
+export let ssr = functions.https.onRequest(app);
